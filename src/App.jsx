0acc6ded4c3e3bd2e5ed6e18712e9b4205bcbc1f
Ryan <glanzrm@gmail.com>
2022-04-29 12:26:04 -0500

    Initial styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,91 @@
 import React from 'react'
 import Dice from './components/Dice'
 
-let dice = []
-for (let i = 1; i <= 10; i++) {
-  const thisDie = {
-    id: i,
-    value: Math.ceil(Math.random() * 6)
-  }
-  dice.push(thisDie)
-}
-
 function App() {
+  // State
+  const [dice, setDice] = React.useState(allNewDice())
+  const [isFinished, setIsFinished] = React.useState(false)
+
+  // Effects
+  React.useEffect(() => {
+    allNewDice()
+  }, [])
+
+
+  // Functions
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        id: i,
+        value: Math.ceil(Math.random() * 6),
+        isFrozen: false
+      })
+    }
+    return newDice
+  }
+
+  function rollDice() {
+    const newDice =  dice.map(item => {
+        let frozenDice
+        if (item.isFrozen) {
+          frozenDice = item
+        } else {
+          frozenDice = {
+            ...item,
+            value: Math.ceil(Math.random() * 6)
+          }
+        }
+
+        return frozenDice
+      })
+      return newDice
+    }
+
+  function freezeDice(diceId) {
+    setDice(prevDice => prevDice.map(item => {
+      const toFreeze = (item.id === diceId) ? !item.isFrozen : item.isFrozen
+          return(
+            {
+              ...item,
+              isFrozen: toFreeze
+            }
+          )
+    }))
+  }
+
+  function checkForFinish() {
+    let finish = true
+    for (let i = 0; i < 10; i++) {
+      if (!dice[i].isFrozen) {
+        finish = false
+      }
+    }
+    return finish
+  }
+
+  function handleClick() {
+    setIsFinished(checkForFinish)
+    if (isFinished) {
+      setDice(allNewDice())
+      setIsFinished(false)
+    } else {
+      setDice(rollDice())
+    }
+  }
+
+  // Element Arrays
   const diceArray = dice.map(die => {
-    return <Dice key={die.id} value={die.value} />
+    return <Dice
+              key={die.id}
+              id={die.id}
+              value={die.value}
+              isFrozen={die.isFrozen}
+              handleClick={freezeDice}
+            />
   })
 
+  // Initialize Components
   return (
     <div className="App">
       <section className="main-container">
@@ -26,7 +97,10 @@ function App() {
         </div>
         
 
-        <button className="roll-btn">Roll</button>
+        <button
+          className="roll-btn"
+          onClick={handleClick}
+        >{isFinished ? 'Play Again' : 'Roll'}</button>
 
       </section>
     </div>
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Dice(props) {
     return(
-        <button className="die">{props.value}</button>
+        <button 
+            className={'die' + (props.isFrozen ? '-frozen' : '')}
+            onClick={() => props.handleClick(props.id)}
+        >{props.value}</button>
     )
 }
 
